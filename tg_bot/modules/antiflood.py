689754344fe50a12104d1334789bdeb763788cfb
JeffreyTackett <35301380+JeffreyTackett@users.noreply.github.com>
2019-04-13 20:51:44 +0530

    Update antiflood.py

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -11,7 +11,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin, c
 from tg_bot.modules.log_channel import loggable
 from tg_bot.modules.sql import antiflood_sql as sql
 
-FLOOD_GROUP = 3
+FLOOD_GROUP = 2
 
 
 @run_async
@@ -76,8 +76,8 @@ def set_flood(bot: Bot, update: Update, args: List[str]) -> str:
                        "\n<b>Admin:</b> {}" \
                        "\nDisabled antiflood.".format(html.escape(chat.title), mention_html(user.id, user.first_name))
 
-            elif amount < 2:
-                message.reply_text("Antiflood has to be either 0 (disabled), or a number bigger than 3!")
+            elif amount < 1:
+                message.reply_text("Antiflood has to be either 0 (disabled), or a number bigger than 2!")
                 return ""
 
             else:
