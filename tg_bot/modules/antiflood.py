0d556aaccd5954c044bcbc02b9c676daa5f93ff9
Knight <jt1055953@swccd.edu>
2019-04-18 16:27:32 +0530

    Update antiflood.py

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -33,10 +33,8 @@ def check_flood(bot: Bot, update: Update) -> str:
     if not should_ban:
         return ""
 
-   
         if member.can_send_messages: 
-            bot.restrict_chat_member(chat.id, int(user_id),
-                                         can_send_messages=False)
+            bot.restrict_chat_member(chat.id,user_id,can_send_messages=False)
         msg.reply_text("I don't like someone sending multiple messages at a time, Use edit option next time. "
                        "kicked!")
 
