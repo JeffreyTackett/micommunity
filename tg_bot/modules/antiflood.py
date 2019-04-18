719039dab64fe78493120b3ea3405f38a92e0715
Knight <jt1055953@swccd.edu>
2019-04-18 14:07:45 +0530

    chat name

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -35,7 +35,7 @@ def check_flood(bot: Bot, update: Update) -> str:
 
     try:
         chat.unban_member(user.id)
-        msg.reply_text("I don't like someone sending multiple messages at a time, Use edit option next time."
+        msg.reply_text("I don't like someone sending multiple messages at a time, Use edit option next time. "
                        "kicked!")
 
         return "<b>{}:</b>" \
