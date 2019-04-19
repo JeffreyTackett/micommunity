2fddd7548596beac70eb2fc8f8bcf6c1f0f5d87d
Knight <jt1055953@swccd.edu>
2019-04-19 10:38:49 +0530

    Create floodmute.py

---
+++ tg_bot/modules/floodmute.py
@@ -0,0 +1,69 @@
+import html
+from typing import Optional, List
+
+from telegram import Message, Chat, Update, Bot, User
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters
+from telegram.ext.dispatcher import run_async
+from telegram.utils.helpers import mention_html
+
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from tg_bot.modules.helper_funcs.string_handling import extract_time
+from tg_bot.modules.log_channel import loggable
+
+
+@run_async
+@bot_admin
+@user_admin
+@loggable
+
+
+
+def mute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id = extract_user(message, args)
+    if not user_id:
+        message.reply_text("I'll need either a username to mute, or reply to someone to be muted.")
+        return ""
+
+    if user_id == bot.id:
+        message.reply_text("I'm not going to mute myself!")
+        return ""
+
+    member = chat.get_member(int(user_id))
+
+    if member:
+        if is_user_admin(chat, user_id, member=member):
+            message.reply_text("How can i mute an admin?!")
+
+        if member.can_send_messages is None or member.can_send_messages:
+            bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
+            message.reply_text("Muted! This user can't talk now.")
+            return "<b>{}:</b>" \
+                   "\n#MUTE" \
+                   "\n<b>Admin:</b> {}" \
+                   "\n<b>User:</b> {}".format(html.escape(chat.title),
+                                              mention_html(user.id, user.first_name),
+                                              mention_html(member.user.id, member.user.first_name))
+
+        else:
+            message.reply_text("This user is already muted!")
+    else:
+        message.reply_text("I can't find this user here!")
+
+    return ""
+
+_mod_name__ = "Muting"
+
+MUTE_HANDLER = CommandHandler("mute", mute, pass_args=True, filters=Filters.group)
+UNMUTE_HANDLER = CommandHandler("unmute", unmute, pass_args=True, filters=Filters.group)
+TEMPMUTE_HANDLER = CommandHandler(["tmute", "tempmute"], temp_mute, pass_args=True, filters=Filters.group)
+
+dispatcher.add_handler(MUTE_HANDLER)
+dispatcher.add_handler(UNMUTE_HANDLER)
+dispatcher.add_handler(TEMPMUTE_HANDLER)
